8b131b54a0a720b8f28d4269aa724484af652649
Leenard Zarate <103997539+leenrd@users.noreply.github.com>
2023-05-31 18:57:45 +0800

    finished all

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,8 +3,12 @@
 const Hero = () => {
   return (
     <main className="flex flex-col justify-center items-center gap-4 title main">
+      <div className="overflow-hidden p-5">
         <h1 className="text-shadow text-9xl font-extrabold header">Eggcellent Eggs</h1>
-        <p className=" text-shadow">Harvested fresh eggs at your doorstep</p>
+      </div>
+      <div className="overflow-hidden p-3">
+        <p className="sub text-shadow text-3xl">Harvested fresh eggs at your doorstep</p>
+      </div>
     </main>
   )
 }
